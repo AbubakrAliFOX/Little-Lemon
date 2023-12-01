aa5fe9dee8fa2d46148d4a9821630c2108df2e42
Abubakr <thefox5000@gmail.com>
2023-12-01 18:08:03 +0300

    Add mock api

--- src/components/BookingPage/BookingForm/index.jsx
+++ src/components/BookingPage/BookingForm/index.jsx
@@ -1,27 +1,20 @@
-import { useState } from "react"
 
 
-export default function BookingForm () {
-    const availableTimes = ['17:00', '17:00', '17:00', '17:00', '17:00', '17:00'];
-    const [formData, setFormData] = useState({date:'', time: '', guests: '', occasion:''});
 
+export default function BookingForm ({formData, setFormData, availableTimes}) {
     const handleChange = (e) => {
         setFormData(previousData => {
             return {...previousData, [e.target.name]: e.target.value}
         })
     }
+    console.log(formData);
 
     return <form >
     <label htmlFor="res-date">Choose date</label>
     <input name="date" onChange={handleChange} type="date" id="res-date" value={formData.date} />
     <label htmlFor="res-time">Choose time</label>
     <select name="time" onChange={handleChange} value={formData.time} id="res-time">
-       <option>17:00</option>
-       <option>18:00</option>
-       <option>19:00</option>
-       <option>20:00</option>
-       <option>21:00</option>
-       <option>22:00</option>
+       {availableTimes.map(el => <option>{el}</option>)}
     </select>
     <label htmlFor="guests">Number of guests</label>
     <input name="guests" onChange={handleChange} value={formData.guests} type="number" placeholder="1" min="1" max="10" id="guests" />
--- src/components/BookingPage/index.jsx
+++ src/components/BookingPage/index.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react"
 import BookingForm from './BookingForm';
 
 export default function BookingPage () {
+    const [availableTimes, setAvailableTimes] = useState(['17:00', '11:00', '17:00', '17:00', '17:00', '17:00']);
+    const [formData, setFormData] = useState({date:'', time: '', guests: '', occasion:''});
+
     return <>
     <h1>This is the Booking Page Bro</h1>
-    <BookingForm />
+    <BookingForm availableTimes={availableTimes} formData={formData} setFormData={setFormData} />
     </>
 }
