a566178c2b11be2ba3683eb40395f7972f385a09
Abubakr <thefox5000@gmail.com>
2023-12-04 14:00:09 +0300

    Add dish filtering feature

--- src/components/MenuPage/index.jsx
+++ src/components/MenuPage/index.jsx
@@ -4,48 +4,91 @@ import { dishes } from "../../utils/dishedDB";
 import { useState } from "react";
 
 export default function MenuPage() {
-//   const displayedDishes = dishes;
+  //   const displayedDishes = dishes;
   const [tag, setTag] = useState("All");
   const handleClick = (e) => {
-    console.dir(e.target);
+    if (e.target.localName === "li") {
+      setTag((prevTag) => (prevTag = e.target.id));
+    } else if (e.target.localName === "a") {
+      setTag((prevTag) => (prevTag = e.target.parentElement.id));
+    }
   };
 
   return (
     <article className="page-padding menu-page">
       <nav>
         <ul>
-          <li id="All" onClick={handleClick}>
+          <li
+            className={tag === "All" && "selected-li"}
+            id="All"
+            onClick={handleClick}
+          >
             <a href="#">All</a>
           </li>
-          <li id="Specialties" onClick={handleClick}>
+          <li
+            className={tag === "Specialties" && "selected-li"}
+            id="Specialties"
+            onClick={handleClick}
+          >
             <a href="#">Specialties</a>
           </li>
-          <li id="Salads" onClick={handleClick}>
+          <li
+            className={tag === "Salads" && "selected-li"}
+            id="Salads"
+            onClick={handleClick}
+          >
             <a href="#">Salads</a>
           </li>
-          <li id="Seafood" onClick={handleClick}>
+          <li
+            className={tag === "Seafood" && "selected-li"}
+            id="Seafood"
+            onClick={handleClick}
+          >
             <a href="#">Seafood</a>
           </li>
-          <li id="Desserts" onClick={handleClick}>
+          <li
+            className={tag === "Desserts" && "selected-li"}
+            id="Desserts"
+            onClick={handleClick}
+          >
             <a href="#">Desserts</a>
           </li>
-          <li id="Soups" onClick={handleClick}>
+          <li
+            className={tag === "Soups" && "selected-li"}
+            id="Soups"
+            onClick={handleClick}
+          >
             <a href="#">Soups</a>
           </li>
-          <li id="Beverages" onClick={handleClick}>
+          <li
+            className={tag === "Beverages" && "selected-li"}
+            id="Beverages"
+            onClick={handleClick}
+          >
             <a href="#">Beverages</a>
           </li>
         </ul>
       </nav>
       <section className="menu-display">
-        {dishes.map((dish) => (
-          <MenuItem
-            key={dish.name}
-            imgUrl={dish.imageUrl}
-            name={dish.name}
-            price={dish.price}
-          />
-        ))}
+        {tag === "All" &&
+          dishes.map((dish) => (
+            <MenuItem
+              key={dish.name}
+              imgUrl={dish.imageUrl}
+              name={dish.name}
+              price={dish.price}
+            />
+          ))}
+        {dishes
+          .filter((el) => el.tag === tag)
+          .map((dish) => (
+            <MenuItem
+              key={dish.name}
+              imgUrl={dish.imageUrl}
+              name={dish.name}
+              price={dish.price}
+            />
+          ))}
       </section>
     </article>
   );
