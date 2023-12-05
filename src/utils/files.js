334b35e45a92d39631f9c54ba02a64ca50d97f7a
Abubakr <thefox5000@gmail.com>
2023-12-05 07:59:05 +0300

    Add burger menu

---
+++ src/utils/files.js
@@ -0,0 +1,134 @@
+const menu = [
+    {
+      name: "Shawarma",
+      price: "$9.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/hjjr5t5xlj3etho1remk.webp",
+      tag: "Specialties"
+    },
+    {
+      name: "Shish Tawouk",
+      price: "$10.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674769/LittleLemon/mdkkatumak7gwashvfhs.webp",
+      tag: "Specialties"
+    },
+    {
+      name: "Stuffed Vine Leaves",
+      price: "$8.49",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/rhrkve3ezohcxevbkjfi.webp",
+      tag: "Specialties"
+    },
+    {
+      name: "Sambosa",
+      price: "$6.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674769/LittleLemon/klsdeznkx6fzxawrci2j.webp",
+      tag: "Specialties"
+    },
+    {
+      name: "Bruschetta",
+      price: "$7.99",
+      imageUrl: "bruschetta.jpg",
+      tag: "Specialties"
+    },
+    {
+      name: "Fattoush",
+      price: "$5.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674770/LittleLemon/fcudcixqgpthgjf5leu1.webp",
+      tag: "Salads"
+    },
+    {
+      name: "Tabbouleh",
+      price: "$6.49",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674769/LittleLemon/nqcmjygg9f7tsd7dagw4.webp",
+      tag: "Salads"
+    },
+    {
+      name: "Greek Salad",
+      price: "$8.99",
+      imageUrl: "greek_salad.jpg",
+      tag: "Salads"
+    },
+    {
+      name: "Mineral Water",
+      price: "$1.99",
+      imageUrl: "mineral_water.jpg",
+      tag: "Beverages"
+    },
+    {
+      name: "Saudi Coffee",
+      price: "$3.49",
+      imageUrl: "saudi_coffee.jpg",
+      tag: "Beverages"
+    },
+    {
+      name: "Black Coffee",
+      price: "$2.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674769/LittleLemon/tj16cociavyzfk4t3ap0.webp",
+      tag: "Beverages"
+    },
+    {
+      name: "Cappuccino",
+      price: "$4.49",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674769/LittleLemon/njg3ho20tmybv8tsi6ol.webp",
+      tag: "Beverages"
+    },
+    {
+      name: "Apple Juice",
+      price: "$3.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/tzme3daqefnjuznyusyk.webp",
+      tag: "Beverages"
+    },
+    {
+      name: "Pomegranate Juice",
+      price: "$4.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/xap4oat092433ut08o94.webp",
+      tag: "Beverages"
+    },
+    {
+      name: "Strawberry Mojito",
+      price: "$5.49",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674769/LittleLemon/bkkmkxes9mlsnbf9e7ea.webp",
+      tag: "Beverages"
+    },
+    {
+      name: "Lentil Soup",
+      price: "$4.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/gpa9bhrfu8bn4jcms3fh.webp",
+      tag: "Soups"
+    },
+    {
+      name: "Creamy Chicken Soup",
+      price: "$5.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674769/LittleLemon/fxn7qwvd6bsza8mykpxf.webp",
+      tag: "Soups"
+    },
+    {
+      name: "Lemon Dessert",
+      price: "$6.49",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/bswgsvmqazeqyzgkwbib.webp",
+      tag: "Desserts"
+    },
+    {
+      name: "Um Ali",
+      price: "$7.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674770/LittleLemon/expajj33oihfixp0yip7.webp",
+      tag: "Desserts"
+    },
+    {
+      name: "Kunafa",
+      price: "$8.49",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674770/LittleLemon/lyydbtsucpgr7yvhxesy.webp",
+      tag: "Desserts"
+    },
+    {
+      name: "Shrimps",
+      price: "$12.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/b6gdurk0x4vc8rsqeuyn.webp",
+      tag: "Seafood"
+    },
+    {
+      name: "Salmon Filet",
+      price: "$14.99",
+      imageUrl: "https://res.cloudinary.com/dewpck9nt/image/upload/v1701674768/LittleLemon/b6gdurk0x4vc8rsqeuyn.webp",
+      tag: "Seafood"
+    }
+  ];
